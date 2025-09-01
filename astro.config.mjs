8820c7a671a45230ea3f820c524c9c6dda4e8556
heygideon <47083178+heygideon@users.noreply.github.com>
2025-09-01 21:09:35 +0100

    feat: add homepage
    - add tailwindcss, prettier
    - add fonts (Work Sans, Satoshi)
    - create homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,59 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Work Sans",
+        cssVariable: "--font-sans",
+        variants: [
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-Regular.woff2"],
+            weight: "400",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-Italic.woff2"],
+            weight: "400",
+            style: "italic",
+          },
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-Semibold.woff2"],
+            weight: "600",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-SemiboldItalic.woff2"],
+            weight: "600",
+            style: "italic",
+          },
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-Bold.woff2"],
+            weight: "700",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/WorkSans/WorkSans-BoldItalic.woff2"],
+            weight: "700",
+            style: "italic",
+          },
+        ],
+      },
+      {
+        provider: fontProviders.fontshare(),
+        name: "Satoshi",
+        cssVariable: "--font-satoshi",
+        weights: ["700", "900"],
+        subsets: ["latin"],
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
