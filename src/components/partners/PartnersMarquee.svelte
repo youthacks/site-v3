fe37dad594ea20bb06ebb60dc08f5205506c10b4
heygideon <47083178+heygideon@users.noreply.github.com>
2025-09-21 23:07:09 +0100

    fix: show sponsor logos in colour on mobile

--- src/components/partners/PartnersMarquee.svelte
+++ src/components/partners/PartnersMarquee.svelte
@@ -61,7 +61,7 @@
       onmouseenter={onHover}
       onmouseleave={onLeave}
       tabindex="-1"
-      class="mr-4 flex-none opacity-50 grayscale transition duration-200 hover:scale-105 hover:opacity-100 hover:grayscale-0 hover:duration-300"
+      class="mr-4 flex-none opacity-50 transition duration-200 hover:scale-105 hover:opacity-100 hover:grayscale-0 hover:duration-300 pointer-fine:grayscale"
     >
       <img
         src={partner.data.logo.src}
